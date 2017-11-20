dc417365a9314b30569e4234bd72d38ce4b9945f
BaranovMykola <mapsg32@gmail.com>
2017-11-20 16:44:01 +0200

    Added bare load balancer

---
+++ Servers/server/routes/index.js
@@ -0,0 +1,299 @@
+const express = require('express');
+var app = express();
+var session = session = require('express-session');
+const router = express.Router();
+//var cookieParser = require('cookie-parser');
+const pg = require('pg');
+const path = require('path');
+var connectionString = "postgres://postgres:1111@127.0.0.1:5432/Tasks";
+var pgClient = new pg.Client(connectionString);
+
+const exec = require("child_process").exec
+
+app.use(session({
+    secret: '2C44-4D44-WppQ38S',
+    resave: true,
+    saveUninitialized: true,
+	
+}));
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+	req.session.store = [];
+	res.render('index.ejs', {user:req.session.user});
+	
+});
+
+app.get('/load', function(req,res){
+	res.render('load.ejs',{user:req.session.user});
+	
+});
+
+app.post('/load', function(req,res){
+//	exec('D:/Studying/Programming/Projects/WebProgramming/Servers/assets/LaserTracking.exe assets/'+req.body.file+' assets/'+req.body.result+'.jpg', function callback(error, stdout, stderr){
+				
+	//});
+	if(!req.session.user)
+	{
+		res.render('error.ejs', {err_mess:"You must be logged in",user:req.session.user});
+	}
+	else
+	{
+			
+		
+		pg.connect(connectionString, (err, client, done) => {
+			if(err) {
+			  done();
+			  console.log(err);
+			  return res.status(500).json({success: false, data: err});
+			}
+			const query = client.query('select * from performance order by threads LIMIT 1');
+			query.on('row', (row) => {
+				
+				s = row.server
+				
+				console.log('user='+req.session.user);
+				console.log('files='+req.body.file.length);
+				console.log('min='+s);
+				
+				
+			});
+			query.on('end', () => {
+			  done();
+				
+							pg.connect(connectionString, (err, client, done) => {
+						if(err) {
+						  done();
+						  console.log(err);
+						  return res.status(500).json({success: false, data: err});
+						}
+						const query = client.query("insert into task (server,start,username,file,files) values("+s+",CURRENT_TIMESTAMP,'"+req.session.user+"',0,"+req.body.file.length+") returning id");
+						query.on('row', (row) => {
+							
+							console.log("returned id = " + row.id);
+							
+							
+						});
+						query.on('end', () => {
+						  done();
+						  
+										  pg.connect(connectionString, (err, client, done) => {
+										if(err) {
+										  done();
+										  console.log(err);
+										  return res.status(500).json({success: false, data: err});
+										}
+										const query = client.query("update performance set threads = threads+1 where server = "+s);
+										query.on('row', (row) => {
+											
+											
+											
+											
+										});
+										query.on('end', () => {
+										  done();
+										  
+										});
+									});
+						  
+						});
+					});
+			  
+			});
+		});
+		
+									
+
+			
+			
+			
+		res.render('profile.ejs', {conf:req.session.store, user:req.session.user});//});
+	}
+});
+
+function minServer()
+{
+	//select * from performance order by threads LIMIT 1
+			pg.connect(connectionString, (err, client, done) => {
+			if(err) {
+			  done();
+			  console.log(err);
+			  return res.status(500).json({success: false, data: err});
+			}
+			const query = client.query('select * from performance order by threads LIMIT 1');
+			query.on('row', (row) => {
+				console.log("min server = " + row.server);
+				return row.server;
+			});
+			query.on('end', () => {
+			  done();
+			  
+			});
+		});
+}
+
+
+app.get('/profile', function(req,res){
+	if(req.session.user)
+	{
+		
+					pg.connect(connectionString, (err, client, done) => {
+			if(err) {
+			  done();
+			  console.log(err);
+			  return res.status(500).json({success: false, data: err});
+			}
+			const query = client.query('select * from performance order by threads LIMIT 1');
+			query.on('row', (row) => {
+				console.log("min server = " + row.server);
+				return row.server;
+			});
+			query.on('end', () => {
+			  done();
+				res.render('profile.ejs', {conf:req.session.store, user:req.session.user});
+			});
+		});
+		
+		
+	}
+	else
+	{
+		res.render('error.ejs', {err_mess:"You must be logged in",user:req.session.user});
+	}
+});
+
+app.post('/login', function (req, res) {
+	var auth = false;
+	if (!req.body.username || !req.body.password) {
+		res.render('login', {user:req.session.user, message: 'You should fill all fields!'});
+	}
+	else 
+	{
+		pg.connect(connectionString, (err, client, done) => {
+		if(err) {
+		  done();
+		  console.log(err);
+		  return res.status(500).json({success: false, data: err});
+		}
+		const query = client.query('SELECT * FROM users where username=($1)', [req.body.username]);
+		query.on('row', (row) => {
+			if(row.password == req.body.password)
+			{
+				auth = true;
+				req.session.user = row.username;
+				req.session.admin = row.admin;
+				req.session.store = [];
+			}
+		});
+		query.on('end', () => {
+		  done();
+		  if(auth == true)
+		  {
+			  res.render('index', {user:req.session.user, admin:req.session.admin});
+		  }
+		  else
+		  {
+			  res.render('login', {user:req.session.user, message: 'Authentication failed'});
+		  }
+		});
+	});	
+}
+
+
+	  
+  
+});
+
+app.get('/login', function(req, res)
+{
+	req.session.destroy();
+	res.render('login.ejs', {user : null, message: null});
+});
+ 
+// Logout endpoint
+app.get('/logout', function (req, res) {
+  req.session.destroy();
+  res.send("logout success!");
+});
+
+app.post('/signup', function (req, res) {
+	var err = false;
+	if (!req.body.username || !req.body.password || !req.body.password_repeat) {
+		res.render('login', {user:req.session.user, message: 'You should fill all fields!'});
+	}
+	else 
+	{
+		var pas1 = req.body.password;
+		var pas2 = req.body.password_repeat;
+		var login = req.body.username;
+		
+		if(pas1 == pas2)
+		{
+			pg.connect(connectionString, (err, client, done) => {
+				if(err) {
+				  done();
+				  console.log(err);
+				  return res.status(500).json({success: false, data: err});
+				}
+				const query = client.query('SELECT * FROM users where username=($1)', [req.body.username]);
+				query.on('row', (row) => {
+					err = true;
+					console.log(row);
+				});
+				query.on('end', () => {
+				  done();
+				  	if(!err)
+					{
+						console.log('okay, create user');
+						createUser(login, pas1, false);
+						console.log('user created?');
+						req.session.user = login;
+						req.session.admin = false;
+						res.render('index', {user:login,admin:req.session.admin});
+					}
+					else
+					{
+						res.render('login', {user:null, message: "Nickname already exists"});
+					}
+				});
+			});
+		}
+		else
+		{
+			res.render('login', {user:null, message: "Passwords are not equals"});
+		}
+		
+	
+		
+	}	
+});
+
+
+function createUser(nick, pass, adm)
+{
+	var str = "INSERT INTO users VALUES('"+nick+"', '"+pass+"', '"+adm+"')";
+				pg.connect(connectionString, (err, client, done) => {
+				if(err) {
+				  done();
+				  console.log(err);
+				  return res.status(500).json({success: false, data: err});
+				}
+				console.log('query start');
+				const query = client.query(str);
+				query.on('row', (row) => {
+				});
+				query.on('end', () => {
+				  done();
+				});
+			});
+}
+
+app.get('/test1', function(req,res)
+{
+	res.redirect(307, 'http://localhost:5000/');
+});
+
+
+module.exports = app;
